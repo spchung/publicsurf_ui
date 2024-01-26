da52bf03fafde1447e8836a577de563e059d7018
Darren Li <darrenli@cherricorp.com>
2024-01-26 17:34:31 +0800

    add photo hover

--- components/Photos/PhotoCard/index.tsx
+++ components/Photos/PhotoCard/index.tsx
@@ -1,28 +1,67 @@
+import { useState, KeyboardEvent, MouseEvent } from 'react';
 import Link from 'next/link';
 import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import { PhotoListItem } from '../types';
+import IconButton from '@mui/material/IconButton';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import style from './PhotoCard.module.scss';
 
 export default function PhotoCard({ id, url, name, userImg }: PhotoListItem) {
+  const [showPrice, setShowPrice] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowPrice(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowPrice(false);
+  };
+
+  const photoHoverContainerStyle = {
+    display: showPrice ? 'flex' : 'none',
+  };
+
+  const handleAddToCart = (e: KeyboardEvent | MouseEvent) => {
+    e.preventDefault();
+    console.log(id);
+  };
+
   return (
     <Link href={`/photo/${id}`}>
       <ImageListItem>
-        <img
-          srcSet={`${url}?w=248&fit=crop&auto=format&dpr=2 2x`}
-          src={`${url}?w=248&fit=crop&auto=format`}
-          alt="poster"
-          loading="lazy"
-        />
-        <Box className={style.photoFooter}>
-          <div className={style.userInfo}>
-            <Avatar alt={name} src={userImg} className={style.userInfo_avatar} />
-            <span>{name}</span>
+        <div
+          className={style.photoMain}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <img
+            srcSet={`${url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            src={`${url}?w=248&fit=crop&auto=format`}
+            alt="poster"
+            loading="lazy"
+          />
+          <div className={style.photoHoverContainer} style={photoHoverContainerStyle}>
+            <span className={style.priceNum}>$100</span>
+            <IconButton
+              size="small"
+              className={style.addToCart}
+              onClick={handleAddToCart}
+              onKeyDown={handleAddToCart}
+            >
+              <AddShoppingCartIcon fontSize="inherit" />
+            </IconButton>
           </div>
-          <div>烏石港北提</div>
-        </Box>
+        </div>
+        <div className={style.photoFooter}>
+          <div className={style.photoFooterInfo}>
+            <div className={style.userInfo}>
+              <Avatar alt={name} src={userImg} className={style.userInfo_avatar} />
+              <span>{name}</span>
+            </div>
+            <div>烏石港北提</div>
+          </div>
+        </div>
       </ImageListItem>
     </Link>
   );
